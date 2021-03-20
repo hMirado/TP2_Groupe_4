2e43021e2f577df861b9564d8e2dbbc3c75062b0
hMirado <miradotsangy@gmail.com>
2021-03-20 17:26:46 +0300

    Personnalisation des cellules

--- TP2_Groupe_4/CountriesTableViewController.swift
+++ TP2_Groupe_4/CountriesTableViewController.swift
@@ -32,13 +32,14 @@ class CountriesTableViewController: UITableViewController {
         // #warning Incomplete implementation, return the number of rows
         return countries.count
     }
-
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CountryCell", for: indexPath)
 
         let country = countries[indexPath.row]
         cell.textLabel?.text = country.name
+        cell.detailTextLabel?.text = country.isoCode
+        cell.imageView?.image = UIImage(named: country.isoCode)
 
         return cell
     }
